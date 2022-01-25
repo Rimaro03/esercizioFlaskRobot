31f3769738b135b95d29e1ec6fc0e46846def648
Rimaro <leonardo.ongaro2003@hotmail.com>
2022-01-25 06:48:42 +0100

    better dashboard

--- main.py
+++ main.py
@@ -25,27 +25,43 @@ mysql.init_app(app)
 # Default route
 @app.route('/')
 def dashboard():    
-    return render_template('dashboard.html', **getData())
+    return render_template('dashboard.html', **getData(1))
 
 
 @socketio.on('getData')
 def sendResponse():
-    dictionary = getData()
+    dictionary = getData(1)
     emit('response', dictionary)
 
+@socketio.on('getMoreData')
+def sendResponse():
+    dictionary = getData(7)
+    emit('response', dictionary)
 
-def getData():
+def getData(limit):
         cursor = mysql.get_db().cursor()
 
         # Query
         cursor.execute(''' SELECT Rover.IDRover, Misura.temperatura,Misura.umidita,Misura.timestamp FROM Misura,Rover,Luogo WHERE (Misura.IDRover = Rover.IDRover) 
-        AND (Misura.IDLuogo = Luogo.IDLuogo) AND (Misura.IDRover = 1) ORDER BY Misura.timestamp DESC LIMIT 1 ''')
+        AND (Misura.IDLuogo = Luogo.IDLuogo) AND (Misura.IDRover = 1) ORDER BY Misura.timestamp DESC LIMIT {}'''.format(limit)) 
         rv = cursor.fetchall()
     
-        dictionary = {
+        if limit==1:
+            dictionary = {
             "temp": int(rv[0][1]), 
             "umi": int(rv[0][2])
         }
+        else:
+            dictionary = {}
+            temp = []
+            timestamp = []
+            for i in range(len(rv)):
+                temp.append(int(rv[i][1]))
+                timestamp.append(rv[i][3])
+            dictionary = {
+                "temp": temp, 
+                "timestamp": timestamp
+            }
         return dictionary
 
 if __name__ == "__main__":
